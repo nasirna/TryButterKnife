b22e28f38acb31895e94240efdb0ac0e6653fbb0
nasirna <mangkubumi16@gmail.com>
2021-01-26 07:39:35 +0700

    menambahkan ButerKnife pada TextView

--- app/src/main/java/com/example/trybutterknife/MainActivity.java
+++ app/src/main/java/com/example/trybutterknife/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.trybutterknife;
 
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tv_belajarbk)
+    TextView tvBelajarBK;
+    @BindString(R.string.salam_bk)
+    String helloBK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        tvBelajarBK.setText(helloBK);
     }
 }
